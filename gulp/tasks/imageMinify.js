2cba053f334ecdfec3169ea40ccd0a31ded78b34
spoonya <nomad8t@gmail.com>
2021-10-24 13:39:47 +0300

    init: project setup

---
+++ gulp/tasks/imageMinify.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+
+module.exports = function imageMinify() {
+  return gulp
+    .src('src/images/**/*.{gif,png,jpg,jpeg,webp}')
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.mozjpeg({
+          quality: 75,
+          progressive: true
+        }),
+        imagemin.optipng({ optimizationLevel: 4 })
+      ])
+    )
+    .pipe(gulp.dest('build/images'));
+};
