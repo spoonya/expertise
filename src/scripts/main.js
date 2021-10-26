8a7c0351e9535f7f2c57883071d7eb408a34187f
spoonya <nomad8t@gmail.com>
2021-10-26 17:14:02 +0300

    fix: add regenerator runtime

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,7 @@
 /* eslint-disable no-unused-vars */
+import 'core-js/es6/promise';
+import 'regenerator-runtime/runtime';
+
 import validateFormCallback from './forms/callback';
 import validateFormQuiz from './forms/quiz';
 import initQuiz from './quiz/quiz';
